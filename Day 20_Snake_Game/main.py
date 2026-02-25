6adfcd6d46009be974c8396866547b6b759d43c0
Borse Andreas <borse.andreas@gmail.com>
2026-02-25 21:24:41 +0200

    Add Snake game logic, food, and scoreboard

--- Day 20_Snake_Game/main.py
+++ Day 20_Snake_Game/main.py
@@ -1,5 +1,7 @@
 from turtle import Screen, Turtle
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 screen = Screen()
 
@@ -9,6 +11,9 @@ screen.title("Snake Game by Andreas-Flaviu Borse")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -21,5 +26,23 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+    #Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    #Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor () < -280 or snake.head.ycor() > 280 or snake.head.ycor() < - 280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    #Detect collision with tail
+    for segment in snake.segments[1:]:
+        # if segment == snake.head:
+        #     pass
+        if snake.head.distance(segment)<10:
+            game_is_on =False
+            scoreboard.game_over()
 
 screen.exitonclick()
